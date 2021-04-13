21a18043b80ee18510fd08a0b5bdd9ef8aea3a98
SamPCunningham <51377712+SamPCunningham@users.noreply.github.com>
2021-04-13 16:56:23 -0400

    started form for update/myinfo

--- frontend/src/Components/MyInfo/myInfoList.js
+++ frontend/src/Components/MyInfo/myInfoList.js
@@ -2,9 +2,66 @@ import React from 'react';
 
 const MyInfoList = () => {
     return ( 
-        
-            <p>There is going to be content here</p>
-        
+        <form>
+            <div className="col-xs-12 col-sm-6">
+                <p><span style={{ color: "red" }}>*</span> Indicates a required field</p>
+            </div>
+            <div class="form-group row">
+	            <label class="col-sm-5 col-md-4 control-label">Social Security #:<span style={{ color: "red"}}>*</span></label>
+			      <div class="col-sm-7 col-md-8">
+			          <input type="text" size="3" value="XXX" disabled />-
+			          <input type="text" size="2" value="XX" disabled />-
+			          <input type="text" size="4" value="XXXX" disabled />
+			      </div>
+	         </div>
+             <div class="form-group row">
+	            <label class="col-sm-5 col-md-4 control-label">First Name:<span style={{ color: "red"}}>*</span></label>
+	            <div class="col-sm-7 col-md-8">
+	                <input type="text" name="fname" />
+	            </div>
+	         </div>
+             <div class="form-group row">
+	            <label class="col-sm-5 col-md-4 control-label">Middle Initial:</label>
+	            <div class="col-sm-7 col-md-8">
+	                <input type="text" name="mi" size="1" maxlength="1" />
+	            </div>
+	         </div>
+             <div class="form-group row">
+	             <label class="col-sm-5 col-md-4 control-label">Last Name:<span style={{ color: "red"}}>*</span></label>
+	             <div class="col-sm-7 col-md-8">
+	                 <input name="lname" type="text" />
+	             </div>
+	         </div>
+             <div class="form-group row">
+	             <label class="col-sm-5 col-md-4 control-label">Email Address:<span style={{ color: "red"}}>*</span></label>
+	             <div class="col-sm-7 col-md-8">
+	                 <input  name="email" type="email" id="email" />
+	             </div>
+	         </div>
+             <div class="form-group row">
+                <label class="col-sm-5 col-md-4 control-label">Date of Birth:<span style={{ color: "red"}}>*</span></label>
+                <select>
+                    <option value="January">Jan</option>
+                    <option value="February">Feb</option>
+                    <option value="March">Mar</option>
+                    <option value="April">Apr</option>
+                    <option value="May">May</option>
+                    <option value="June">Jun</option>
+                    <option value="July">Jul</option>
+                    <option value="August">Aug</option>
+                    <option value="September">Sep</option>
+                    <option value="October">Oct</option>
+                    <option value="November">Nov</option>
+                    <option value="December">Dec</option>
+                </select>
+                <select>
+                    
+                </select>
+             </div>
+
+
+
+        </form>
      );
 }
  
--- frontend/src/Components/pages.js
+++ frontend/src/Components/pages.js
@@ -15,10 +15,12 @@ export function Home() {
     );
 }
 
+ 
+
 export function MyInfo() {
     return (
-        <div>
-           <MyInfoList /> 
+        <div className="container" style={{ marginTop: "40px"}}>
+            <MyInfoList /> 
         </div>
         
     );
@@ -26,19 +28,20 @@ export function MyInfo() {
 
 export function MyExams() {
     return (
-        <div>
-            <h1>Certifications and Exams</h1>
+        <div className="container" style={{ marginTop: "40px"}}>
+            <HomeTable />
+            
         </div>
     );
 }
 
 export function IndustryInfo() {
     return (
-        <div>
+        <div className="container col-xs-12 col-sm-10 col-md-8 col-lg-7" style={{ marginTop: "40px", textAlign: "left" }}>
             <p><strong>Type I Certification </strong>- Technicians receiving a passing grade on the Type I (small appliance) examination are certified to recover refrigerant during the maintenance, service or repair of packaged terminal air conditioners with 5 pounds or less of refrigerant. Only Type I or Universal certified technicians can recover refrigerant from these units</p>
             <p><strong>Type II Certification </strong>- Technicians receiving a passing grade on the Type II (high-pressure and very high pressure) examination are certified to recover refrigerant during the maintenance, service or repair of high-pressure equipment (High-Pressure R-12, R-114, R-134a, R-401A and R-500; Higher-Pressure R-22, R-502, R-402A, R-402B, R-404A, R-407A, R-407B, and R-407C; and Very-High-Pressure R-410A, R-410B, R-13, R-23, and R-503). Only Type II or Universal certified technicians can recover refrigerant from these units.</p>
             <p><strong>Type III Certification </strong>- Technicians receiving a passing grade on the Type III (low-pressure appliance) examination are certified to recover refrigerant during the maintenance, service or repair of low-pressure equipment (CFC-11, HCFC-123). Only Type III or Universal certified technicians can recover refrigerant from these units.</p>
-            <p><strong>Universal Certification </strong></p>- Technicians receiving a Universal Certification are certified to recover refrigerant during the maintenance, service or repair of small appliances, high-pressure equipment and low-pressure equipment. That is, they are certified to work on any type of air conditioning and refrigeration equipment, except motor vehicle air conditioning (see Section 609 MVAC Certification).
+            <p><strong>Universal Certification </strong>- Technicians receiving a Universal Certification are certified to recover refrigerant during the maintenance, service or repair of small appliances, high-pressure equipment and low-pressure equipment. That is, they are certified to work on any type of air conditioning and refrigeration equipment, except motor vehicle air conditioning (see Section 609 MVAC Certification).</p>
         </div>
     );
 }
